cad4c8f65b10b6f0b01639e7277847c583e3f0d0
Mar <mar@Mars-MacBook-Pro.local>
2022-07-28 23:17:53 +0100

    Try this

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
